c823f7e62d53f3495b5ec7863c5ca9fac4465568
Mark Anthony Perez <mark@act2.com.au>
2024-09-27 13:09:08 +0800

    commit

--- obj/Debug/net6.0/FurrLife.AssemblyInfo.cs
+++ obj/Debug/net6.0/FurrLife.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FurrLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d64e4cba3205e447c8b00f7293236337e6516fcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d615792048b49dcfb87f3b8d644509e972c1d328")]
 [assembly: System.Reflection.AssemblyProductAttribute("FurrLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FurrLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
