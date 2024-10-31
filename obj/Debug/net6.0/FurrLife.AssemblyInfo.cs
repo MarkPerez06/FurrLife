3e7363f161918455434c0142572b8852024e9a6c
Mark Anthony Perez <mark@act2.com.au>
2024-10-31 13:35:27 +0800

    commit bin

--- obj/Debug/net6.0/FurrLife.AssemblyInfo.cs
+++ obj/Debug/net6.0/FurrLife.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FurrLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aca4ed4882e7151dbf65c6ffcf7a217a3dd7c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71cf4110746ae591026632baa59997fc4cca955a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FurrLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FurrLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
